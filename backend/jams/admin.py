801afb0b203f2d42c9b5ddf245f830308fbd0fa1
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-06-15 14:26:26 +0200

    Delete backend directory

--- backend/jams/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Jam, JamClientSession, JamEvent, JamSnapshot, JamTransaction
-
-admin.site.register([Jam, JamTransaction, JamEvent, JamSnapshot, JamClientSession])
